289876382319e67b5ed92e969aac79b069dab865
retumika <takutoxo17@gmail.com>
2026-03-04 06:53:06 +0900

    important: 脆弱性の解消

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
 import { getFirestore, collection, getDocs, query, orderBy } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCrcK4mO10-c16ZLMxWvHOer00ml_6iy7A",
-    authDomain: "portfolio001-b2897.firebaseapp.com",
-    projectId: "portfolio001-b2897",
-    storageBucket: "portfolio001-b2897.firebasestorage.app",
-    messagingSenderId: "367472258585",
-    appId: "1:367472258585:web:4773a4af05102faba30488"
-};
+import { firebaseConfig } from './env.js';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
