63f0e4a58c648c362522008e7d13f965537dea6c
retumika <takutoxo17@gmail.com>
2026-03-09 04:50:34 +0900

    パーティクル追加

--- script.js
+++ script.js
@@ -37,6 +37,68 @@ async function loadNews() {
     }
 }
 
+//背景の演出
+const canvas = document.getElementById('particle-canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particlesArray;
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + Math.random() * 100;
+        this.size = Math.random() * 30;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+        this.color = 'rgba(' + (Math.random() * 255) + ', ' + (Math.random() * 255) + ', ' + (Math.random() * 255) + ', ' + (Math.random() * 0.5 + 0.2) + ')';
+    }
+
+    // 座標の更新
+    update() {
+        this.y -= this.speedY; // 上に移動（Y座標を減らす）
+        this.x += this.speedX; // 少し横に揺らす
+        if (this.y < 0 - this.size) {
+            this.y = canvas.height + this.size;
+            this.x = Math.random() * canvas.width;
+        }
+    }
+
+    // 描画
+    draw() {
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+function init() {
+    particlesArray = [];
+    let numberOfParticles = 20;
+    for (let i = 0; i < numberOfParticles; i++) {
+        particlesArray.push(new Particle());
+    }
+}
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for (let i = 0; i < particlesArray.length; i++) {
+        particlesArray[i].update();
+        particlesArray[i].draw();
+    }
+
+    requestAnimationFrame(animate);
+}
+
+init();
+animate();
+
+
+
 window.showPage = function (pageId) {
     const pages = document.querySelectorAll('.page');
     pages.forEach(page => {
